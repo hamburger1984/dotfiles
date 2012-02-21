cc871521642c4f5d0ad215662fcde8f21f7ee9db
Andreas Krohn <hamburger1984@gmail.com>
2012-02-21 09:12:51 +0100

    added -b (build updated projects) -f (if -b is defined, build everything) to pull_everything.sh

--- pull_everything.sh
+++ pull_everything.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
+build=0
+force=0
+while getopts "bf" o
+do
+	case "$o" in
+		"b")
+			build=1
+			;;
+		"f")
+			force=1
+			;;
+	esac
+done
+
 for dir in $(find -L . -maxdepth 4 -xtype d -type d -iname .git | sort); do
 	echo ${dir%/.git}
 	pushd ${dir%/.git} >/dev/null
 	otxt=$(git pull origin master)
 	ocode=$?
 	echo "$otxt"
-	if [ $ocode -eq 0 ] && [[ ! "$otxt" == *"up-to-date"* ]] && [ -d "./build" ]; then
-		pushd "./build" >/dev/null
+	if [ $build -eq 1 ] && [ $ocode -eq 0 ] && [ -d "./build" ]; then
+		if [ $force -eq 1 ] || [[ ! "$otxt" == *"up-to-date"* ]]; then
+			pushd "./build" >/dev/null
 
-		if [ -e "./build.sh" ]; then
-			sh ./build.sh
-		else
-			cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ..; make -j -l 1.5
+			if [ -e "./build.sh" ]; then
+				sh ./build.sh
+			else
+				cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ..; make -j -l 1.5
+			fi
+
+			popd >/dev/null
 		fi
-
-		popd >/dev/null
 	fi
 	popd >/dev/null
 	echo 
@@ -26,16 +42,18 @@ for dir in $(find -L . -maxdepth 2 -xtype d -type d -iname .svn | cut -d / -f 2 
 	otxt=$(svn up .)
 	ocode=$?
 	echo "$otxt"
-	if [ $ocode -eq 0 ] && [[ "$otxt" == *"Aktualisiert"* ]] && [ -d "./build" ]; then
-		pushd "./build" >/dev/null
+	if [ $build -eq 1 ] && [ $ocode -eq 0 ] && [ -d "./build" ]; then
+		if [ $force -eq 1 ] || [[ "$otxt" == *"Aktualisiert"* ]]; then
+			pushd "./build" >/dev/null
 
-		if [ -e "./build.sh" ]; then
-			sh ./build.sh
-		else
-			cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ..; make -j -l 1.5
+			if [ -e "./build.sh" ]; then
+				sh ./build.sh
+			else
+				cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ..; make -j -l 1.5
+			fi
+
+			popd >/dev/null
 		fi
-
-		popd >/dev/null
 	fi
 	popd >/dev/null
 	echo
