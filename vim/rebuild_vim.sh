6b393b7eb562ee453d20fbbf877ea8cce7408655
Andreas Krohn <Hamburger1984@gmail.com>
2014-12-14 13:40:25 +0100

    some vim stuff

--- vim/rebuild_vim.sh
+++ vim/rebuild_vim.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
+# --------------------------------------------
+# hg clone https://vim.googlecode.com/hg/ vim
+# cd vim
+# --------------------------------------------
+
 echo "----- PULL"
-git pull
+hg pull && hg update tip
 
 echo "----- CONFIGURE"
-# ./configure --with-features=huge --enable-python3interp --enable-luainterp --enable-multibyte --prefix=$HOME/opt
-./configure --with-features=huge --enable-python3interp --enable-luainterp --enable-multibyte --enable-gui=gtk2 --prefix=/home/andreas/opt
+./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-pythoninterp \
+    --enable-python3interp \
+    --enable-luainterp \
+    --enable-gui=gtk2 \
+    --enable-cscope \
+    --prefix=/home/andreas/opt
 
 echo "----- COMPILE, INSTALL"
 make && make install
 
 echo "----- VERSION"
 $HOME/opt/bin/vim --version
-
-#echo "----- LN to $HOME/bin"
-#for f in vim vimtutor gvim ; do
-#    if [ -f $HOME/bin/$f ]; then
-#        rm -f $HOME/bin/$f
-#    fi
-#    ln -s $HOME/opt/bin/$f $HOME/bin/$f
-#done
 
