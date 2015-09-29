333ac9642c8af2c010c289cb038f94a7d58ca6eb
Andreas Krohn <Hamburger1984@gmail.com>
2015-09-29 17:12:33 +0200

    pushd/popd isn't installed everywhere.. stick to cd instead

--- vim/init_vim.sh
+++ vim/init_vim.sh
@@ -18,19 +18,16 @@ dir=`pwd`
 
 # fetch plug.vim to $HOME/.vim/autoload
 mkdir -p "$HOME/.vim/autoload/"
-pushd "$HOME/.vim/autoload/" > /dev/null
+cd "$HOME/.vim/autoload/"
 wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-popd > /dev/null
 
 
-pushd "$HOME" > /dev/null
+cd "$HOME"
 ln -s "$dir/vimrc" ".vimrc"
 ln -s "$dir/gvimrc" ".gvimrc"
-popd > /dev/null
 
-pushd "$HOME/.vim/" > /dev/null
+cd "$HOME/.vim/" > /dev/null
 ln -s "$dir/vim_plugins.vim" "plugins.vim"
 ln -s "$dir/extras" "extras"
-popd > /dev/null
 
-echo "start vim and run :PlugInstall<CR> now.."
+echo "START VIM AND RUN :PlugInstall<CR> NOW.."
