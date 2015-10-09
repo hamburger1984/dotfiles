4444fa33d2432a81be63084128ad7f2b23e1efb6
Andreas Krohn <Hamburger1984@gmail.com>
2015-10-09 15:49:34 +0200

    init_vim.sh kind of works in windows bash as well..

--- vim/init_vim.sh
+++ vim/init_vim.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 
-if [ -d ~/.vim ]; then
-    rm -rf ~/.vim
-fi
+files=("$HOME/.vimrc" "$HOME/.gvimrc")
+for f in "${files[@]}"; do
+    if [ -f "$f" ]; then
+        echo "removing $f"
+        rm -f "$f"
+    fi
+done
 
-if [ -f ~/.vimrc ]; then
-    rm -f ~/.vimrc
+if [ "`uname -o`" == "Msys" ]; then
+    folder="$HOME/vimfiles"
+else
+    folder="$HOME/.vim"
 fi
-
-if [ -f ~/.gvimrc ]; then
-    rm -f ~/.gvimrc
+if [ -d "$folder" ]; then
+    echo "removing $folder"
+    rm -rf "$folder"
 fi
-
 
 dir=`pwd`
 
 
 # fetch plug.vim to $HOME/.vim/autoload
-mkdir -p "$HOME/.vim/autoload/"
-cd "$HOME/.vim/autoload/"
-wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+mkdir -p "$folder/autoload/"
+cd "$folder/autoload/"
+echo "fetching plug.vim to $folder/autoload/"
+curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
+    --output plug.vim
 
 
 cd "$HOME"
+echo "linking [g]vimrc to $HOME"
 ln -s "$dir/vimrc" ".vimrc"
 ln -s "$dir/gvimrc" ".gvimrc"
 
-cd "$HOME/.vim/" > /dev/null
-ln -s "$dir/vim_plugins.vim" "plugins.vim"
+cd "$folder"
 ln -s "$dir/extras" "extras"
 
 echo "START VIM AND RUN :PlugInstall<CR> NOW.."
