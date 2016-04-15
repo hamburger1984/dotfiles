75e77515ab0a4c6937dbaea7828404544b161b27
Andreas Krohn <Hamburger1984@gmail.com>
2016-04-15 13:26:08 +0200

    Vim:  * added visual page percent plugin  * build with perl  * added a patch to prevent perl-enabled build to fail

--- build/rebuild_vim.sh
+++ build/rebuild_vim.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#     --enable-pythoninterp=dynamic \
+#     --enable-python3interp=dynamic \
+
+#     --enable-perlinterp=no \
+#     --enable-pythoninterp=no \
+#     --enable-python3interp=yes \
+#     --enable-rubyinterp=yes \
+#     --with-tlib=ncurses \
+
 echo "----- CONFIGURE"
 ./configure \
     --enable-fail-if-missing \
@@ -8,23 +17,16 @@ echo "----- CONFIGURE"
     --enable-gpm \
     --enable-gui=gtk3 \
     --enable-luainterp=dynamic \
+    --enable-perlinterp=yes \
     --enable-pythoninterp=dynamic \
     --enable-python3interp=dynamic \
     --enable-tclinterp=dynamic \
     --with-features=huge \
     --with-luajit \
     --with-tlib=ncurses \
-    --prefix=/home/andreas/opt
-
-#     --enable-perlinterp=no \
-#     --enable-pythoninterp=no \
-#     --enable-python3interp=yes \
-#     --enable-rubyinterp=yes \
-#     --with-tlib=ncurses \
-
-echo "----- COMPILE, INSTALL"
-make -j -l 4 && make install
-
-echo "----- VERSION"
-which vim
+    --prefix=/home/andreas/opt &&
+echo "----- COMPILE, INSTALL" &&
+make -j -l 4 && make install &&
+echo "----- VERSION" &&
+which vim &&
 vim --version
