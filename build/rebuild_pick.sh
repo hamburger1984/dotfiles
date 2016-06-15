0db832499b38a7fdf89b60959b57aa06d9f30847
Andreas Krohn <Hamburger1984@gmail.com>
2016-06-15 09:35:39 +0200

    Neovim -- Just discovered :terminal, I'm in love. Added a <Leader><Esc> to exit terminal mode. pick -- install globally (was user only). requirements.txt -- removed pep8, flake8, pyflakes .. updated others.

--- build/rebuild_pick.sh
+++ build/rebuild_pick.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-PREFIX="$HOME/opt"
+# PREFIX="$HOME/opt"
+# ./autogen.sh && \
+#     ./configure --exec_prefix="$PREFIX" --prefix="$PREFIX" && \
+#     make -j && \
+#     make install
+
 ./autogen.sh && \
-    ./configure --exec_prefix="$PREFIX" --prefix="$PREFIX" && \
-    make -j && \
-    make install
+    ./configure && \
+    make -j -l 4.0 && \
+    sudo make install
